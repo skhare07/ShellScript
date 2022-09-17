9db24351c28514d07ec3349bbdfcbad47ce4ea4a
Sourav Khare <souravkhare34@gmail.com>
2022-09-17 13:25:12 +0530

    [Sourav]- Program to show daily wages along with total wage

---
+++ empWageFunctionArrayUC8.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function calculateWorkingHours() {
+
+	case $1 in 
+0)
+	workingHours=0;
+	;;
+
+1)
+	workingHours=1;
+	;;
+2)
+	workingHours=2;
+	;;
+
+esac
+	echo $workingHours;
+
+}
+
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHours=0;
+day=1;
+
+function getEmpWage()
+{
+local empHr=$1;
+echo $(( $empHr*$perHourSalary ));
+
+
+}
+
+
+	while [[ $day -le 20 && $totalWorkingHours -lt 100 ]]
+do
+	empHrs=$( calculateWorkingHours $(( RANDOM%3 )) );
+	totalWorkingHours=$(( $totalWorkingHours + $empHrs ));
+
+	if [ $totalWorkingHours -gt 100 ]
+	then
+	totalWorkingHours=$(( $totalWorkingHours - $empHrs ));
+	break;
+	fi
+
+	salary=$(( $empHrs * $perHourSalary ));
+	totalSalary=$(( $totalSalary  + $salary ));
+
+	(( day++ ))
+
+dailyWage[$day]=$( getEmpWage $empHrs )
+
+done
+
+	echo Employee has earned $totalSalary rupees in $totalWorkingHours hours;
+
+echo "Day-Wise salary : ${dailyWage[@]}";
